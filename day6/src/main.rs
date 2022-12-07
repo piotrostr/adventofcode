49d89052fb74a05c135755b41fc442322a049108
Piotr Ostrowski <piotrostr@google.com>
2022-12-07 00:04:27 +0000

    pass second part of the questiong

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -15,4 +15,16 @@ fn main() {
         }
         index += 1;
     }
+
+    let mut index: usize = 14;
+    while index != input.len() {
+        let window = chars[(index - 14)..index].to_vec();
+        // use a HashSet to find if all of the numbers are unique
+        let set = HashSet::<&char>::from_iter(window.iter());
+        if set.len() == 14 {
+            println!("first fourteen unique at: {}", index);
+            break;
+        }
+        index += 1;
+    }
 }
