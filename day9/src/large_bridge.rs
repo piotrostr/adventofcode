b6f4a5df092cd7e313cf80c9c05c103aaf5443c6
Piotr Ostrowski <piotrostr@google.com>
2022-12-28 02:50:50 +0100

    implement primary attempt, go back to the smaller input and add a method for visualization

--- day9/src/large_bridge.rs
+++ day9/src/large_bridge.rs
@@ -1,5 +1,10 @@
+use crate::common::check_if_are_adjacent;
+
 use super::common::{Direction, Instruction, Point};
-use std::collections::{HashSet, VecDeque};
+use std::{
+    collections::{HashSet, VecDeque},
+    mem::swap,
+};
 
 /// there is no head position and no tail, as the head is just going to be the `self.knots.back()`
 /// and tail `self.knots.front()`
@@ -11,13 +16,17 @@ pub struct LargeBridge {
 impl LargeBridge {
     pub fn new() -> Self {
         Self {
-            knots: VecDeque::<Point>::from(vec![Point { x: 0, y: 0 }]),
+            knots: VecDeque::<Point>::from(
+                (0..=9)
+                    .map(|_| Point { x: 0, y: 0 })
+                    .collect::<Vec<Point>>(),
+            ),
             tail_positions_set: HashSet::<Point>::new(),
         }
     }
 
     pub fn unique_positions(&self) -> usize {
-        0
+        self.tail_positions_set.len()
     }
 
     /// This is going to be slightly different from the previous implementation, every knot is
@@ -28,16 +37,102 @@ impl LargeBridge {
     pub fn handle_instruction(&mut self, instruction: &Instruction) {
         let mut steps_remaining = instruction.n_steps;
         while steps_remaining > 0 {
+            let head_position = *self.knots.back().unwrap();
             match instruction.direction {
-                Direction::Up => {}
-                Direction::Down => {}
-                Direction::Left => {}
-                Direction::Right => {}
+                Direction::Right => {
+                    *self.knots.back_mut().unwrap() = Point {
+                        x: head_position.x + 1,
+                        y: head_position.y,
+                    };
+                }
+                Direction::Left => {
+                    *self.knots.back_mut().unwrap() = Point {
+                        x: head_position.x - 1,
+                        y: head_position.y,
+                    };
+                }
+                Direction::Up => {
+                    *self.knots.back_mut().unwrap() = Point {
+                        x: head_position.x,
+                        y: head_position.y + 1,
+                    };
+                }
+                Direction::Down => {
+                    *self.knots.back_mut().unwrap() = Point {
+                        x: head_position.x,
+                        y: head_position.y - 1,
+                    };
+                }
+            }
+
+            // the error is here, I implemented the logic here as in the snake game
+            //
+            // for this application, the knots are stretching in a similar direction as the head,
+            // but not following it directly, more-so trying to hold the line
+            let mut iterator = self.knots.iter_mut().rev();
+            let mut knot_one = iterator.next().unwrap();
+            let mut new_position = head_position;
+            while iterator.len() > 0 {
+                let knot_two = iterator.next().unwrap();
+                // the method below might be redundant altogether for the sake of incrementing it
+                // as in the example
+                //
+                // only the last knot before the head follows directly, similar as tail in the
+                // previous part of the question
+                //
+                // the rest of the knots are just trying to hold the line
+                if !check_if_are_adjacent(knot_one, knot_two) {
+                    swap(&mut (*knot_two), &mut new_position);
+                }
+                knot_one = knot_two;
             }
 
             steps_remaining -= 1;
 
             self.tail_positions_set.insert(*self.knots.front().unwrap());
+
+            self.visualize();
         }
+    }
+
+    pub fn visualize(&self) {
+        let mut min_x = 0;
+        let mut max_x = 0;
+        let mut min_y = 0;
+        let mut max_y = 0;
+
+        for knot in self.knots.iter() {
+            if knot.x < min_x {
+                min_x = knot.x;
+            }
+            if knot.x > max_x {
+                max_x = knot.x;
+            }
+            if knot.y < min_y {
+                min_y = knot.y;
+            }
+            if knot.y > max_y {
+                max_y = knot.y;
+            }
+        }
+
+        for y in (min_y..=max_y).rev() {
+            for x in min_x..=max_x {
+                let point = Point { x, y };
+                if self.knots.contains(&point) {
+                    if point == *self.knots.front().unwrap() {
+                        print!("T");
+                    } else if point == *self.knots.back().unwrap() {
+                        print!("H");
+                    } else {
+                        print!("X");
+                    }
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        println!();
     }
 }
